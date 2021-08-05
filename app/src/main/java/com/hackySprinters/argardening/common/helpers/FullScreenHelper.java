a7e0af4608a9bb7a37b82ad0d6c45f4a76d4827f
Tanmay Darmorha <tanmaydarmorha@gmail.com>
2021-08-05 22:49:07 +0530

    Add Helpers

--- app/src/main/java/com/hackySprinters/argardening/common/helpers/FullScreenHelper.java
+++ app/src/main/java/com/hackySprinters/argardening/common/helpers/FullScreenHelper.java
@@ -1,4 +1,21 @@
 package com.hackySprinters.argardening.common.helpers;
 
+import android.app.Activity;
+import android.view.View;
+
 public class FullScreenHelper {
+    public static void setFullScreenOnWindowFocusChanged(Activity activity, boolean hasFocus) {
+        if (hasFocus) {
+            activity
+                    .getWindow()
+                    .getDecorView()
+                    .setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
 }
