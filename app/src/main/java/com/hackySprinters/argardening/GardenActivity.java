b67c326f198e7b5d537cba0f6d12a151a987bb13
Kapil Sharma <kapilsharma@Kapils-MacBook-Air.local>
2021-08-05 22:09:07 +0530

    initial commit

---
+++ app/src/main/java/com/hackySprinters/argardening/GardenActivity.java
@@ -0,0 +1,14 @@
+package com.hackySprinters.argardening;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GardenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/hackySprinters/argardening/common/helpers/InstantPlacementSettings.java
@@ -0,0 +1,4 @@
+package com.hackySprinters.argardening.common.helpers;
+
+public class InstantPlacementSettings {
+}
